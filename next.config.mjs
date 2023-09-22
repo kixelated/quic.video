314b438ea64a35da77eceaa67a5a108ac48774aa
Luke Curley <kixelated@gmail.com>
2023-09-22 14:36:52 -0700

    Static pages working with next.js
    aka the easy part.

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+// Import necessary modules
+import withMDX from "@next/mdx"
+import remarkGfm from "remark-gfm"
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	// Configure pageExtensions to include md and mdx
+	pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+	// Optionally, add any other Next.js config below
+	reactStrictMode: true,
+}
+
+const mdxConfig = withMDX({
+	extension: /\.mdx?$/,
+	options: {
+		remarkPlugins: [remarkGfm],
+	},
+})
+
+// Export the configuration
+export default mdxConfig(nextConfig)
