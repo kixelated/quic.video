b050fa00341fec99bf470f892298fb004a1eb812
Luke Curley <kixelated@gmail.com>
2024-12-02 10:39:44 -0800

    Port over the Astro front-end from moq-js.

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM oven/bun:latest
+
+WORKDIR /app
+
+COPY package.json bun.lockb ./
+RUN bun ci --production
+
+COPY . /app
+
+FROM base AS prod-deps
+RUN --mount=type=cache,id=npm,target=/app/.npm \
+	npm set cache /app/.npm && \
+	npm ci --production
+
+FROM base AS build
+RUN --mount=type=cache,id=npm,target=/app/.npm \
+	npm set cache /app/.npm && \
+	npm ci
+RUN npm run build
+
+FROM base
+COPY --from=prod-deps /app/node_modules /app/node_modules
+COPY --from=build /app/web/dist /app/web/dist
+
+ENV HOST="0.0.0.0"
+CMD [ "node", "./web/dist/server/entry.mjs" ]
---
+++ astro.config.mjs
@@ -0,0 +1,64 @@
+import fs from "node:fs"
+import path from "node:path"
+import mdx from "@astrojs/mdx"
+import nodejs from "@astrojs/node"
+import solidJs from "@astrojs/solid-js"
+import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
+
+import mkcert from "vite-plugin-mkcert"
+import topLevelAwait from "vite-plugin-top-level-await"
+import wasm from "vite-plugin-wasm"
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		mdx(),
+		solidJs(),
+		tailwind({
+			// Disable injecting a basic `base.css` import on every page.
+			applyBaseStyles: false,
+		}),
+	],
+	// Renders any non-static pages using node
+	adapter: nodejs({
+		mode: "standalone",
+	}),
+	// Default to static rendering, but allow server rendering per-page
+	output: "hybrid",
+	vite: {
+		build: {
+			target: "esnext",
+		},
+		base: "./",
+		server: {
+			// HTTPS is required for SharedArrayBuffer
+			https: true,
+			fs: {
+				allow: [
+					".",
+					// Allow `bun link`
+					fs.realpathSync(path.resolve("node_modules/@kixelated/moq")),
+				],
+			},
+		},
+		plugins: [
+			// Generates a self-signed certificate using mkcert
+			mkcert(),
+
+			wasm(),
+
+			topLevelAwait(),
+		],
+		worker: {
+			plugins: [wasm(), topLevelAwait()],
+		},
+		resolve: {
+			alias: {
+				"@": "/src",
+			},
+		},
+	},
+	// Don't add trailing slashes to paths
+	trailingSlash: "never",
+})
