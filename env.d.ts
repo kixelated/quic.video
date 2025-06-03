5ed76e1873297b530b02173877c2b414bdb43e53
Luke Curley <kixelated@gmail.com>
2025-06-03 16:35:38 -0700

    fix

--- env.d.ts
+++ env.d.ts
@@ -1,11 +1,11 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-	readonly PUBLIC_RELAY_SCHEME: "http" | "https"
-	readonly PUBLIC_RELAY_HOST: string
-	readonly PUBLIC_DEMO_TOKEN: string
+	readonly PUBLIC_RELAY_SCHEME: "http" | "https";
+	readonly PUBLIC_RELAY_HOST: string;
+	readonly PUBLIC_DEMO_TOKEN: string;
 }
 
 interface ImportMeta {
-	readonly env: ImportMetaEnv
+	readonly env: ImportMetaEnv;
 }
