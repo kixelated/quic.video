107b6c3f8224890fc0f4b2390ff9bed81c99a44f
Luke Curley <kixelated@gmail.com>
2025-06-25 11:12:31 -0700

    Use (well-typed) Web Components instead of the JS API.
    Directly exposing the SolidJS components has been deprecated.

--- src/components/publish.tsx
+++ src/components/publish.tsx
@@ -1,7 +1,7 @@
-import { Connection, Publish, Support } from "@kixelated/hang";
 import { adjectives, animals, uniqueNamesGenerator } from "unique-names-generator";
 
-import { Show, createEffect, onCleanup } from "solid-js";
+import "@kixelated/hang/support/element";
+import "@kixelated/hang/publish/element";
 
 export default function () {
 	const name = uniqueNamesGenerator({ dictionaries: [adjectives, animals], separator: "-" });
@@ -9,43 +9,9 @@ export default function () {
 		`${import.meta.env.PUBLIC_RELAY_SCHEME}://${import.meta.env.PUBLIC_RELAY_HOST}/anon/${name}.hang`,
 	);
 
-	const preview = (
-		<video style={{ "max-width": "100%", height: "100%", margin: "0 auto", "border-radius": "1rem" }} autoplay muted />
-	) as HTMLVideoElement;
-
-	const connection = new Connection({ url });
-
-	const broadcast = new Publish.Broadcast(connection, {
-		enabled: true,
-		video: true,
-		audio: {
-			// Request the browser to do some noise suppression for microphone input.
-			noiseSuppression: true,
-			echoCancellation: true,
-		},
-	});
-
-	createEffect(() => {
-		const media = broadcast.video.media.get();
-		if (!media || !preview) return;
-
-		preview.srcObject = new MediaStream([media]);
-		onCleanup(() => {
-			preview.srcObject = null;
-		});
-	});
-
-	onCleanup(() => {
-		connection.close();
-		broadcast.close();
-	});
-
 	return (
 		<div>
-			{/* biome-ignore lint/a11y/useValidAriaRole: false-positive */}
-			<Support.Modal role="publish" show="partial" />
-
-			<Publish.Controls broadcast={broadcast} />
+			<hang-support prop:mode="publish" prop:show="partial" />
 
 			<h3>Watch URL:</h3>
 			<a href={`/watch/${name}`} rel="noreferrer" target="_blank" class="ml-2">
@@ -53,9 +19,13 @@ export default function () {
 			</a>
 
 			<h3>Preview:</h3>
-			<Show when={broadcast.device.get()} fallback={<span class="italic">No device selected</span>}>
-				{preview}
-			</Show>
+			<hang-publish prop:url={url} prop:controls={true} prop:video={true} prop:audio={true}>
+				<video
+					style={{ "max-width": "100%", height: "100%", margin: "0 auto", "border-radius": "1rem" }}
+					autoplay
+					muted
+				/>
+			</hang-publish>
 		</div>
 	);
 }
--- src/components/watch.tsx
+++ src/components/watch.tsx
@@ -1,6 +1,6 @@
-import { Connection, Support } from "@kixelated/hang";
-import { Watch } from "@kixelated/hang";
-import { onCleanup } from "solid-js";
+// Use the hang web components.
+import "@kixelated/hang/support/element";
+import "@kixelated/hang/watch/element";
 
 export default function (props: { name: string; token?: string }) {
 	// The signed token is only needed for the demo/ prefix just to prevent abuse.
@@ -8,32 +8,14 @@ export default function (props: { name: string; token?: string }) {
 	const url = new URL(
 		`${import.meta.env.PUBLIC_RELAY_SCHEME}://${import.meta.env.PUBLIC_RELAY_HOST}/${props.token ? `${props.token}.jwt` : `${props.name}.hang`}`,
 	);
-	const canvas = (
-		<canvas style={{ "max-width": "100%", height: "auto", margin: "0 auto", "border-radius": "1rem" }} />
-	) as HTMLCanvasElement;
-
-	const connection = new Connection({ url });
-	const broadcast = new Watch.Broadcast(connection, { path: "", enabled: true });
-	const video = new Watch.VideoRenderer(broadcast.video, { canvas });
-	const audio = new Watch.AudioEmitter(broadcast.audio, { muted: true });
-
-	let root!: HTMLDivElement;
-
-	onCleanup(() => {
-		connection.close();
-		broadcast.close();
-		video.close();
-		audio.close();
-	});
 
 	return (
-		<div ref={root}>
-			{/* biome-ignore lint/a11y/useValidAriaRole: false-positive */}
-			<Support.Modal role="watch" show="partial" />
+		<>
+			<hang-support prop:mode="watch" prop:show="partial" />
 
-			{canvas}
-
-			<Watch.Controls broadcast={broadcast} video={video} audio={audio} root={root} />
-		</div>
+			<hang-watch prop:url={url} prop:muted={true} prop:controls={true}>
+				<canvas style={{ "max-width": "100%", height: "auto", margin: "0 auto", "border-radius": "1rem" }} />
+			</hang-watch>
+		</>
 	);
 }
